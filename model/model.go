6fe7eca31583918f013f633741837cfc09118e66
Parichay Singh <61328236+Parichay-Singh@users.noreply.github.com>
2021-10-09 21:03:41 +0530

    Added model

---
+++ model/model.go
@@ -0,0 +1,23 @@
+package model
+
+import (
+	"time"
+)
+
+// API data Struct
+// For User
+type User struct {
+	Id       string `json:"id" bson:"_id"`
+	Name     string `json:"name" bson:"name"`
+	Email    string `json:"email" bson:"email"`
+	Password string `json:"password" bson:"password"`
+}
+
+// For Post
+type Post struct {
+	Id        string    `json:"id" bson:"_id"`
+	Caption   string    `json:"caption" bson:"caption"`
+	Image_URL string    `json:"image_url" bson:"image_url"`
+	TimeStamp time.Time `json:"timestamp" bson:"timestamp"`
+	UserID    string    `json:"userid" bson:"_userid"`
+}
